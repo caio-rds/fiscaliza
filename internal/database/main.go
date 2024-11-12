2baccf7ac44a5d08f11d04bb778a3ae3f0e9b123
caiords <caiodtn@gmail.com>
2024-11-11 23:21:43 -0300

    FIX: PostGreSQL comes back.

--- internal/database/main.go
+++ internal/database/main.go
@@ -2,19 +2,23 @@ package database
 
 import (
 	"fiscaliza/internal/models"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
+	"os"
 )
 
 func ConnectDB() *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("fiscaliza.db"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
-		log.Fatalln("failed to connect database: %v", err)
+		log.Fatalf("failed to connect database: %v\n", err)
 	}
 	err = db.AutoMigrate(&models.User{}, &models.Report{}, &models.Recovery{}, &models.Address{})
 	if err != nil {
-		log.Fatalln("failed to migrate database: %v", err)
+		log.Fatalf("failed to migrate database: %v\n", err)
 	}
 
 	return db
